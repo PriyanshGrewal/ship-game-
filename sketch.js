3a938d4c64101ea5e79a17ad99b0a8ca164eb81a
PriyanshGrewal <88784482+PriyanshGrewal@users.noreply.github.com>
2021-08-16 16:16:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var sea;
+function preload(){
+seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+  seaImg.addImage(sea.png);
+}
+
+
+
+
